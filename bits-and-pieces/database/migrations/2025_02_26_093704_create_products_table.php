c47828de6576fe3734010321f0ca0a4a7e4e8265
filiplyrheden <filip.lyrheden@gmail.com>
2025-02-26 10:37:53 +0100

    Added Product model, controller, and migration for products table

---
+++ bits-and-pieces/database/migrations/2025_02_26_093704_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('console');
+            $table->string('producer');
+            $table->string('color');
+            $table->integer('weight');
+            $table->string('connection');
+            $table->decimal('price', 5, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};

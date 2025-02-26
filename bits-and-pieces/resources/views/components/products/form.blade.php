0dbe61c95ce0c65983037b9e0ad36f41c39a52b3
filiplyrheden <filip.lyrheden@gmail.com>
2025-02-26 12:45:42 +0100

    Reorder product form fields for improved layout and accessibility

--- bits-and-pieces/resources/views/components/products/form.blade.php
+++ bits-and-pieces/resources/views/components/products/form.blade.php
@@ -3,14 +3,11 @@
 <label for="name">Name</label>
 <input type="text" name="name" id="name" value="{{ old('name', $product->name ?? '') }}">
 
-<label for="description">Description</label>
-<textarea type="text" name="description" id="description" value="{{ old('description', $product->description ?? '') }}"></textarea>
-
 <label for="console">Console</label>
 <input type="text" name="console" id="console" value="{{ old('console', $product->console ?? '') }}">
 
-<label for="color">Color</label>
-<input type="text" name="color" id="color" value="{{ old('color', $product->color ?? '') }}">
+<label for="description">Description</label>
+<textarea type="text" name="description" id="description" value="{{ old('description', $product->description ?? '') }}"></textarea>
 
 <label for="connection">Connection</label>
 <select id="connection" name="connection" value="{{ old('connection', $product->connection ?? '') }}">

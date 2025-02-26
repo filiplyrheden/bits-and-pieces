7aa9ef7a00a7988d467348b3e137d089ca1fec9e
filiplyrheden <filip.lyrheden@gmail.com>
2025-02-26 12:03:34 +0100

    Fix product form connection field to retain selected value on submission

--- bits-and-pieces/resources/views/components/products/form.blade.php
+++ bits-and-pieces/resources/views/components/products/form.blade.php
@@ -13,11 +13,10 @@
 <input type="text" name="color" id="color" value="{{ old('color', $product->color ?? '') }}">
 
 <label for="connection">Connection</label>
-<select id="connection" name="connection">
+<select id="connection" name="connection" value="{{ old('connection', $product->connection ?? '') }}">
     <option value="wireless">Wireless</option>
     <option value="wired">Wired</option>
 </select>
-<input type="submit" name="connection" id="connection" value="{{ old('connection', $product->connection ?? '') }}">
 
 <label for="price">Price</label>
 <input type="text" name="price" id="price" value="{{ old('price', $product->price ?? '') }}">

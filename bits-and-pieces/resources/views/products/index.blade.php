366ee70b2e1f50aae857e878aed9f790c63bd2c7
filiplyrheden <filip.lyrheden@gmail.com>
2025-03-06 11:58:16 +0100

    Display product price in the index view

--- bits-and-pieces/resources/views/products/index.blade.php
+++ bits-and-pieces/resources/views/products/index.blade.php
@@ -103,6 +103,7 @@
     @foreach ($products as $product)
     <h2><a href="{{ route('products.show', $product->id) }}">{{ $product->name }}</a></h2>
     <p>{{ $product->description }}</p>
+    <p>{{ $product->price }}</p>
     @endforeach
     {{ $products->links('vendor/pagination/simple-default') }}
     @else
